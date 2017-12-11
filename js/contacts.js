a191c1bbd982408b85e2f610084d74b7fccaf32c
Tanida <tsenjavska@gmail.com>
2017-12-11 21:12:43 +0200

    2017/12/11_3

--- js/contacts.js
+++ js/contacts.js
@@ -1,7 +1,7 @@
 function myMap() {
     var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
+        center:new google.maps.LatLng(56.9501901, 24.1020694),
+        zoom:15,
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
     }
--- js/register.js
+++ js/register.js
@@ -1,6 +1,8 @@
-$('#inputPassword3, #inputPassword4').on('keyup', function () {
+$("#registrationForm").on('submit', function (e) {
+    e.preventDefault()
     if ($('#inputPassword3').val() == $('#inputPassword4').val()) {
         console.info('matching');
     } else 
     console.info('not matching');
   });
+
