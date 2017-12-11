5e03164320abfbc8585b538140cd3f40c23d0e8f
Tanida <tsenjavska@gmail.com>
2017-12-11 23:55:33 +0200

    register update

--- js/register.js
+++ js/register.js
@@ -3,6 +3,24 @@ $("#registrationForm").on('submit', function (e) {
     if ($('#inputPassword3').val() == $('#inputPassword4').val()) {
         console.info('matching');
     } else 
-    console.info('not matching');
+    alert('The passwords are not matching');
+    $(this).onsubmit();
   });
+
+//   $("#registrationForm").on('submit', function (e) {
+//     e.preventDefault()
+//     if ($('#inputPassword3').length() > 8){
+//         console.info('matching');
+//     } else 
+//     alert('You have entered less than 8 characters for password');
+//     $(this).onsubmit();
+//   });
+
+  $('#inputPassword3').on('blur', function(){
+    if(this.value.length < 8){ // checks the password value length
+       alert ('You have entered less than 8 characters for password');
+    //   $(this).focus(); // focuses the current field.
+    return false;
+     }
+     }) // stops the execution.
 
