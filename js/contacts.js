a61a7ed01fa74e70b6b493e081381c2ccf9cd5f6
Tanida <tsenjavska@gmail.com>
2017-12-13 18:24:43 +0200

    Chat added

--- js/contacts.js
+++ js/contacts.js
@@ -1,7 +1,7 @@
 function myMap() {
     var mapProp= {
         center:new google.maps.LatLng(56.9501901, 24.1020694),
-        zoom:15,
+        zoom:18,
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
     }
